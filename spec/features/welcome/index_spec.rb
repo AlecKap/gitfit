3003eb3b9de61cafe6213624ed36f0cc0248f87e
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-12-05 12:23:23 -0700

    test: Add aditional test for links on welcome page

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,11 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Welcome Index Page' do
-  describe 'As a visitor' do
-    it 'I see a welcome message' do
+  describe 'As a visitor, when I visit the welcome page' do
+    before :each do
       visit root_path
+    end
 
+    it 'I see a welcome message' do
       expect(page).to have_content("Welcome to GitFit")
+    end
+
+    it 'has a link to create a new movement' do
+      expect(page).to have_link("Record a Movement")
+    end
+
+    it 'when I click the link to record a movement, I am taken to a new movemnet page' do
+      click_link("Record a Movement")
+
+      expect(current_path).to eq(new_movement_path)
+    end
+
+    it 'has a link to view all movements' do
+      expect(page).to have_link("View All Movements")
+    end
+
+    it 'when I click the link to view all movements, I am taken to the movemnets index page' do
+      click_link("View All Movements")
+
+      expect(current_path).to eq(movements_path)
     end
   end
 end
