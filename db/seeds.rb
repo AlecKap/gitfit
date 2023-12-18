ac4a44e04f14c64dce2d392ba67ebdf75af5bc53
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-12-18 11:54:26 -0700

    Add seed data for Movement model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+workouts = [ "pushups", "situps", "squats" ]
+
+5.times do
+  Movement.create!(
+    name_of_mover: "Alec",
+    type_of_movement: workouts.sample,
+    reps: rand(1..100),
+    weight: rand(1..50),
+    created_at: Time.now - rand(1..30).days,
+    updated_at: Time.now - rand(1..30).days,
+  )
+end
+
+5.times do
+  Movement.create!(
+    name_of_mover: "Crow",
+    type_of_movement: workouts.sample,
+    reps: rand(1..100),
+    weight: rand(1..50),
+    created_at: Time.now - rand(1..30).days,
+    updated_at: Time.now - rand(1..30).days,
+  )
+end
+
+5.times do
+  Movement.create!(
+    name_of_mover: "Thomas",
+    type_of_movement: workouts.sample,
+    reps: rand(1..100),
+    weight: rand(1..50),
+    created_at: Time.now - rand(1..30).days,
+    updated_at: Time.now - rand(1..30).days,
+  )
+end
