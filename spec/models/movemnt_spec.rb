de97b477df6b5f212a8137d1ebf22d03a07e3477
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-12-18 11:54:18 -0700

    test: Add test for newest_first method

--- spec/models/movemnt_spec.rb
+++ spec/models/movemnt_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Movement, type: :model do
     it { should validate_presence_of :type_of_movement }
     it { should validate_presence_of :reps }
   end
+
+  describe 'class methods' do
+    it 'can sort by newest first' do
+      expect(Movement.newest_first).to eq(Movement.order(created_at: :desc))
+    end
+  end
 end
