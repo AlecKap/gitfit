55bbab95ba912c460de92f9a1a96d77cc3644d4f
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-12-18 10:38:40 -0700

    Update redirect_to to use turbo_stream in MovementsController

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -12,7 +12,7 @@ class MovementsController < ApplicationController
     movement = Movement.new(movement_params)
     if movement.save
       flash[:notice] = "Movement successfully recorded!"
-      redirect_to movements_path
+      render turbo_stream: turbo_visit(movements_path)
     else
       flash[:alert] = "Please fill in all required fields. You may leave Weight field blank if using just body weight."
       redirect_to new_movement_path
