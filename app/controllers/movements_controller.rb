7c5deb11a121884a98964d12f4ee5363d75a732b
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-12-19 12:17:49 -0700

    Refactor movement filtering and ordering in MovementsController

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -1,32 +1,39 @@
 class MovementsController < ApplicationController
   
   def index
-    # binding.pry
     @movements = Movement.all
+
+    filter_params = params.permit(:name_of_mover, :type_of_movement)
+    filtered_movements = Movement.all
+
+    filter_params.each do |param, value|
+      filtered_movements = filtered_movements.where(param => value) if value.present?
+    end
+
     @ordered_movements = case params[:order]
-      when 'name_of_mover'
-        Movement.order(name_of_mover: :asc)
-      when 'name_of_mover_desc'
-        Movement.order(name_of_mover: :desc)
-      when 'type_of_movement'
-        Movement.order(type_of_movement: :asc)
-      when 'type_of_movement_desc'
-        Movement.order(type_of_movement: :desc)
-      when 'reps'
-        Movement.order(reps: :asc)
-      when 'reps_desc'
-        Movement.order(reps: :desc)
-      when 'weight'
-        Movement.order(weight: :asc)
-      when 'weight_desc'
-        Movement.order(weight: :desc)
-      when 'created_at'
-        Movement.order(created_at: :asc)
-      when 'created_at_desc'
-        Movement.order(created_at: :desc)
-      else
-        Movement.newest_first
-      end
+    when 'name_of_mover'
+      filtered_movements.order(name_of_mover: :asc)
+    when 'name_of_mover_desc'
+      filtered_movements.order(name_of_mover: :desc)
+    when 'type_of_movement'
+      filtered_movements.order(type_of_movement: :asc)
+    when 'type_of_movement_desc'
+      filtered_movements.order(type_of_movement: :desc)
+    when 'reps'
+      filtered_movements.order(reps: :asc)
+    when 'reps_desc'
+      filtered_movements.order(reps: :desc)
+    when 'weight'
+      filtered_movements.order(weight: :asc)
+    when 'weight_desc'
+      filtered_movements.order(weight: :desc)
+    when 'created_at'
+      filtered_movements.order(created_at: :asc)
+    when 'created_at_desc'
+      filtered_movements.order(created_at: :desc)
+    else
+      filtered_movements.newest_first
+  end
   end
   
   def new
